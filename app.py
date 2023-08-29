f898e4a83dfd8654205dbab47728cdd189740842
GgnoHuang <lshapeddesk@icloud.com>
2023-08-29 21:33:45 +0800

    Add protection rule to main branch.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app=Flask(__name__)
 app.config["JSON_AS_ASCII"]=False
 app.config["TEMPLATES_AUTO_RELOAD"]=True
 
-# Pages
+# Pages 
 @app.route("/")
 def index():
 	return render_template("index.html")
